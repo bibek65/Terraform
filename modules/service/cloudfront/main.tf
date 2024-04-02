cd1ef4ff5574b707d48c0e68a6bdc5aa6976e4eb
Bibek Kumar Labh <47681234+bibek65@users.noreply.github.com>
2024-04-02 20:54:14 +0545

    Update main.tf

--- modules/service/cloudfront/main.tf
+++ modules/service/cloudfront/main.tf
@@ -94,7 +94,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 }
 
 resource "aws_cloudfront_origin_access_control" "s3_distribution" {
-  name                              = "Bibek OAC S3"
+  name                              = "${var.oac_name}-OAC"
   description                       = "OAC setup for S3_OAC"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
